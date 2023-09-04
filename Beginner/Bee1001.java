e4f33af27755dc0c91448a4933efb40c9f5aaa34
thiago.dsm <133548984+ethior0@users.noreply.github.com>
2023-09-03 22:19:12 -0300

    Update Bee1001.java

--- Beginner/Bee1001.java
+++ Beginner/Bee1001.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+public class Soma {
+	public static void main (String[] args) {
+	Scanner ler = new Scanner(System.in);
+	
+		int A, B, X;
+		
+		A = ler.nextInt();
+		B = ler.nextInt();
+		
+		X = A+B;
+		
+		System.out.println("X = "+X);
+		ler.close();
+		
+	}
+}
