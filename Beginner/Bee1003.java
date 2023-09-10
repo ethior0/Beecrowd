bb8c89fc6a177faeeccf8961a729496e89e9c5e3
ethior0 <thiagomeigmoreira18@gmail.com>
2023-09-10 00:40:14 -0300

    Simple Sum

---
+++ Beginner/Bee1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bee1003 {
+	public static void main (String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int A, B, SOMA;
+		
+		A = ler.nextInt();
+		B = ler.nextInt();
+		
+		SOMA = A+B;
+		
+		System.out.println("SOMA = "+SOMA);
+		ler.close();
+		
+	}
+}
